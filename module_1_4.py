d8b1660d42f6f89c6ddcabaff5a6aa37a06ca5ab
boksenenko <boksenenko@gmail.com>
2024-11-21 17:29:29 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+#1
+
+my_string = input( "Пишу любую фразу: ")
+print(len(my_string))
+
+#2
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ' , ''))
+print(my_string[0])
+print(my_string[-1])
