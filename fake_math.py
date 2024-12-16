88276816d7cbbdc6bfcfcd8018445f229d3da939
boksenenko <boksenenko@gmail.com>
2024-12-16 20:40:38 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second == 0:
+        print('Error')
+    else:
+        print(first / second)
+
---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+import true_math as tm
+import fake_math as fm
+
+result1 = tm.divide(10, 5)
+result2 = tm.divide(10, 0)
+result3 = fm.divide(10, 5)
+result4 = fm.divide(10, 0)
